fd76b17a401fe4652ec2a2766aeea6b0f2abbe85
niladri <niladri@192.168.1.17>
2019-04-05 01:03:21 +0530

    modified

--- account/src/test/java/org/openedusoft/account/AccountApplicationTests.java
+++ account/src/test/java/org/openedusoft/account/AccountApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ibm.isl.healthcare.product;
+package org.openedusoft.account;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProductApplicationTests {
+public class AccountApplicationTests {
 
 	@Test
 	public void contextLoads() {
